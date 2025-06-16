4f7575b2f11aa458536840c7b341c08d254bb23a
SKitttz <skittz.dev@gmail.com>
2025-06-16 20:40:17 -0300

    chore: small adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  },
-    markdown: {
-    shikiConfig: {
-      theme: 'dracula',
-    },
   },
 });
