ad7ce4a02e56a220d939353aba4a051a9f148c81
SKitttz <skittz.dev@gmail.com>
2025-06-17 20:16:11 -0300

    feat: add resume and adjustments styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [tailwind()],
