c8612fa199de9f8c91e2eccbc8bf4736e4ca4766
skitttz <skittz.dev@gmail.com>
2025-06-19 14:15:07 -0300

    chore: add astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [tailwind(),mdx()],
+  integrations: [tailwind(),mdx(),icon()],
   output: 'server',
   adapter: vercel({webAnalytics: true}),
   
