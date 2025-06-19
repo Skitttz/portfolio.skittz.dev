6ca9d1a6e5111d95de93ae11a12081d7b485f088
skitttz <skittz.dev@gmail.com>
2025-06-19 03:13:52 -0300

    chore: add lib mdx astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),mdx()],
   output: 'server',
   adapter: vercel({webAnalytics: true}),
+  
 });
