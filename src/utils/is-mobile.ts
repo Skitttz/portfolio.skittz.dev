82eef8baa575ccb2e0911f0aa28963b9e3e2c958
skitttz <skittz.dev@gmail.com>
2025-08-17 01:41:33 -0300

    chore: adjust script and test agent

--- src/utils/is-mobile.ts
+++ src/utils/is-mobile.ts
@@ -1,5 +1,6 @@
 function isMobile() {
-  return /Mobi|Android|iPhone|iPad|iPod|Mobile|webOS|BlackBerry|Windows Phone/i.test(navigator.userAgent);
+  return /Mobi|Android|iPhone|iPad|iPod|Mobile|webOS|BlackBerry|Windows Phone/i.test(navigator.userAgent) ||
+    (navigator.maxTouchPoints && navigator.maxTouchPoints > 1);
 }
 
 export { isMobile }
