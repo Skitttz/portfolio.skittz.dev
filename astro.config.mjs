2c0e9851435ef569314dce181e8fdabc8f65ab76
skitttz <skittz.dev@gmail.com>
2026-05-31 19:21:07 -0300

    chore: update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
@@ -7,7 +6,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon(), react()],
+  integrations: [mdx(), icon(), react()],
   output: 'server',
   adapter: vercel({webAnalytics: true}),
   i18n: {
